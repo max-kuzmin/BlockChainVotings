f38b2e23636e8ef77754f8613c12ba739b5387d8
Maxim Kuzmin <MaxGsomGsom@users.noreply.github.com>
2016-04-29 13:56:09 +0000

    Частично реализован сервер

---
+++ BlockChainVotings/MainForm.cs
@@ -0,0 +1,73 @@
+﻿using NetworkCommsDotNet;
+using NetworkCommsDotNet.Connections;
+using NetworkCommsDotNet.Connections.TCP;
+using NetworkCommsDotNet.Tools;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Virgil.Crypto;
+using Virgil.Crypto.Foundation;
+using System.Net;
+
+namespace BlockChainVotings
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            //поиск пиров
+            PeerDiscovery.OnPeerDiscovered += PeerDiscovered;
+            PeerDiscovery.EnableDiscoverable(PeerDiscovery.DiscoveryMethod.UDPBroadcast);
+            PeerDiscovery.DiscoverPeersAsync(PeerDiscovery.DiscoveryMethod.UDPBroadcast);
+
+            //создать серв
+            NetworkComms.AppendGlobalIncomingPacketHandler<string>("Message", PrintIncomingMessage);
+            Connection.StartListening(ConnectionType.TCP, new IPEndPoint(IPAddress.Any, 10000));
+
+
+            //создать соединение и отправить сообщение
+            ConnectionInfo connInfo = new ConnectionInfo("192.168.0.36", 10000);
+            Connection newTCPConn = TCPConnection.GetConnection(connInfo);
+            newTCPConn.SendObject<string>("Message", "it works");
+
+            
+
+
+            //подписать-проверить файл
+            VirgilKeyPair pair = CryptoHelper.GenerateKeyPair();
+            string sign = CryptoHelper.Sign("hello world", pair.PrivateKey());
+            bool res = CryptoHelper.Verify("hello world", sign, pair.PublicKey());
+            MessageBox.Show(Convert.ToBase64String(pair.PublicKey()) + "\n\n\n" + Convert.ToBase64String(pair.PrivateKey()) + "\n\n\n" + sign + "\n\n\n" + res.ToString());
+        }
+
+        //когда нашли пир
+        private void PeerDiscovered(ShortGuid peerIdentifier, Dictionary<ConnectionType, List<EndPoint>> discoveredListenerEndPoints)
+        {
+            MessageBox.Show("\nEndpoints discovered for peer with networkIdentifier " + peerIdentifier);
+            
+        }
+
+        //обработка входящего сообщения
+        private void PrintIncomingMessage(PacketHeader packetHeader, Connection connection, string incomingObject)
+        {
+            MessageBox.Show("\nA message was received from " + connection.ToString() + " which said '" + incomingObject + "'.");
+        }
+
+        //хеш
+        public string CalcSHA256(byte[] data)
+        {
+            byte[] result = VirgilHash.Sha256().Hash(data);
+            return Convert.ToBase64String(result);
+        }
+
+    }
+}
---
+++ BlockChainVotingsTracker/Messages.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChainVotingsTracker
+{
+    public class Message
+    {
+        public MessageType Type {
+            get { return type; }
+        }
+
+        protected MessageType type;
+    }
+
+
+    public class PeerHashMessage: Message
+    {
+        public string PeerHash { get; }
+
+        public PeerHashMessage(string peerHash)
+        {
+            this.type = MessageType.PeerHash;
+            this.PeerHash = peerHash;
+        }
+    }
+
+    public class PeerDisconnectMessage : Message
+    {
+        public string PeerHash { get; }
+
+        public PeerDisconnectMessage(string peerHash)
+        {
+            this.type = MessageType.PeerDisconnect;
+            this.PeerHash = peerHash;
+        }
+    }
+
+    public class PeersMessage : Message
+    {
+        public List<string> PeersAdresses { get; }
+
+        public PeersMessage(List<string> peersAdresses)
+        {
+            this.type = MessageType.Peers;
+            this.PeersAdresses = peersAdresses;
+        }
+    }
+
+    public class RequestPeersMessage : Message
+    {
+        public int Count { get; }
+
+        public RequestPeersMessage(int count)
+        {
+            this.type = MessageType.RequestPeers;
+            this.Count = count;
+        }
+    }
+
+
+    public class RequestBlocksMessage : Message
+    {
+        public List<string> Hashes { get; }
+
+        public RequestBlocksMessage(List<string> hashes)
+        {
+            this.type = MessageType.RequestBlocks;
+            this.Hashes = hashes;
+        }
+    }
+
+    public class RequestTransactionsMessage : Message
+    {
+        public List<string> Hashes { get; }
+
+        public RequestTransactionsMessage(List<string> hashes)
+        {
+            this.type = MessageType.RequestTransactions;
+            this.Hashes = hashes;
+        }
+    }
+
+
+    public class ToPeerMessage : Message
+    {
+        public string SenderHash { get; }
+        public string RecieverHash { get; }
+        public Message Message { get; }
+
+        public ToPeerMessage(string senderHash, string recieverHash, Message message)
+        {
+            this.type = MessageType.MessageToPeer;
+            this.SenderHash = senderHash;
+            this.RecieverHash = recieverHash;
+            this.Message = message;
+        }
+    }
+
+
+    public class TransactionsMessage : Message
+    {
+        public TransactionsMessage()
+        {
+            this.type = MessageType.Transactions;
+        }
+    }
+
+    public class BlocksMessage : Message
+    {
+        public BlocksMessage()
+        {
+            this.type = MessageType.Transactions;
+        }
+    }
+
+
+    public class ConnectToPeerWithTrackerMessage : Message
+    {
+        public string SenderHash { get; }
+        public string RecieverHash { get; }
+
+        public ConnectToPeerWithTrackerMessage(string senderHash, string recieverHash)
+        {
+            this.type = MessageType.ConnectToPeerWithTracker;
+            this.SenderHash = senderHash;
+            this.RecieverHash = recieverHash;
+        }
+    }
+
+
+
+
+
+}
