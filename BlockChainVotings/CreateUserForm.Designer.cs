a6730ebff77675fe28587f217ff5c9474ae5859d
Maxim Kuzmin <MaxGsomGsom@users.noreply.github.com>
2016-05-15 11:08:56 +0000

    создание пользователей, проверка корня

---
+++ BlockChainVotings/CreateUserForm.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace BlockChainVotings
+{
+    partial class CreateUserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxPublicKey = new System.Windows.Forms.TextBox();
+            this.textBoxPrivateKey = new System.Windows.Forms.TextBox();
+            this.labelPublicKey = new System.Windows.Forms.Label();
+            this.labelPrivateKey = new System.Windows.Forms.Label();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.buttonCreateUser = new System.Windows.Forms.Button();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelID = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxPublicKey
+            // 
+            this.textBoxPublicKey.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxPublicKey.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBoxPublicKey.Location = new System.Drawing.Point(84, 12);
+            this.textBoxPublicKey.Name = "textBoxPublicKey";
+            this.textBoxPublicKey.Size = new System.Drawing.Size(213, 20);
+            this.textBoxPublicKey.TabIndex = 0;
+            this.textBoxPublicKey.TextChanged += new System.EventHandler(this.textBoxPublicKey_TextChanged);
+            // 
+            // textBoxPrivateKey
+            // 
+            this.textBoxPrivateKey.Location = new System.Drawing.Point(84, 48);
+            this.textBoxPrivateKey.Name = "textBoxPrivateKey";
+            this.textBoxPrivateKey.Size = new System.Drawing.Size(213, 20);
+            this.textBoxPrivateKey.TabIndex = 1;
+            this.textBoxPrivateKey.TextChanged += new System.EventHandler(this.textBoxPublicKey_TextChanged);
+            // 
+            // labelPublicKey
+            // 
+            this.labelPublicKey.AutoSize = true;
+            this.labelPublicKey.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelPublicKey.Location = new System.Drawing.Point(12, 15);
+            this.labelPublicKey.Name = "labelPublicKey";
+            this.labelPublicKey.Size = new System.Drawing.Size(54, 13);
+            this.labelPublicKey.TabIndex = 2;
+            this.labelPublicKey.Text = "PublicKey";
+            // 
+            // labelPrivateKey
+            // 
+            this.labelPrivateKey.AutoSize = true;
+            this.labelPrivateKey.Location = new System.Drawing.Point(12, 51);
+            this.labelPrivateKey.Name = "labelPrivateKey";
+            this.labelPrivateKey.Size = new System.Drawing.Size(58, 13);
+            this.labelPrivateKey.TabIndex = 3;
+            this.labelPrivateKey.Text = "PrivateKey";
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Location = new System.Drawing.Point(319, 15);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(75, 49);
+            this.buttonGenerate.TabIndex = 4;
+            this.buttonGenerate.Text = "Generate KeyPair";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // buttonCreateUser
+            // 
+            this.buttonCreateUser.Location = new System.Drawing.Point(142, 176);
+            this.buttonCreateUser.Name = "buttonCreateUser";
+            this.buttonCreateUser.Size = new System.Drawing.Size(107, 23);
+            this.buttonCreateUser.TabIndex = 5;
+            this.buttonCreateUser.Text = "Create User";
+            this.buttonCreateUser.UseVisualStyleBackColor = true;
+            this.buttonCreateUser.Click += new System.EventHandler(this.buttonCreateUser_Click);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(84, 88);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(213, 20);
+            this.textBoxName.TabIndex = 6;
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBoxName_TextChanged);
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(84, 126);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(213, 20);
+            this.textBoxID.TabIndex = 7;
+            this.textBoxID.TextChanged += new System.EventHandler(this.textBoxID_TextChanged);
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(12, 91);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 8;
+            this.labelName.Text = "Name";
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Location = new System.Drawing.Point(12, 129);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(18, 13);
+            this.labelID.TabIndex = 9;
+            this.labelID.Text = "ID";
+            // 
+            // CreateUserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 222);
+            this.Controls.Add(this.labelID);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.buttonCreateUser);
+            this.Controls.Add(this.buttonGenerate);
+            this.Controls.Add(this.labelPrivateKey);
+            this.Controls.Add(this.labelPublicKey);
+            this.Controls.Add(this.textBoxPrivateKey);
+            this.Controls.Add(this.textBoxPublicKey);
+            this.Name = "CreateUserForm";
+            this.Text = "CreateUserForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxPublicKey;
+        private System.Windows.Forms.TextBox textBoxPrivateKey;
+        private System.Windows.Forms.Label labelPublicKey;
+        private System.Windows.Forms.Label labelPrivateKey;
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.Button buttonCreateUser;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelID;
+    }
+}
--- BlockChainVotings/VotingsUser.cs
+++ BlockChainVotings/VotingsUser.cs
@@ -11,22 +11,23 @@ namespace BlockChainVotings
         static public string PublicKey = (new Random((int)(DateTime.Now.Ticks)).Next().ToString());
         static public string PrivateKey = (new Random((int)(DateTime.Now.Ticks)).Next().ToString());
         static public string RootPublicKey;
-        static public DateTime RootUserDate;
+        static public string RootCreationSignature;
+        static public DateTime RootUserDate = new DateTime(2016, 1, 1);
 
 
         static public void Login(string password) 
         {
-
+            throw new NotImplementedException();
         }
 
         static public void SaveUser(string publicKey, string privateKey, string password)
         {
-
+            throw new NotImplementedException();
         }
 
         static public void ClearUserData()
         {
-            
+            throw new NotImplementedException();
         }
     }
 }
